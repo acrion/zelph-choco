5b2f7ab3a841cb5865346f58f2622426c891d074
Stefan Zipproth <s.zipproth@acrion.ch>
2026-07-03 02:13:12 +0200

    v0.9.6

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'zelph'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://github.com/acrion/zelph/releases/download/v0.9.5/zelph-windows.zip'
-$checksum    = '074cf6ac96484a3c64011790fef90d1b6e99d795a4ad8a98c0e917ec9847ad2a'
+$url         = 'https://github.com/acrion/zelph/releases/download/v0.9.6/zelph-windows.zip'
+$checksum    = 'a50bc015bedd44f5aba8eafa24a4e0dea1d4129007d4a81d6e7bdd10ae56de95'
 
 Install-ChocolateyZipPackage `
   -PackageName $packageName `
@@ -13,8 +13,10 @@ Install-ChocolateyZipPackage `
   -Checksum64 $checksum `
   -ChecksumType64 'sha256'
 
+<#
 $testsExe = Join-Path $toolsDir 'zelph_tests.exe'
 & $testsExe
 if ($LASTEXITCODE -ne 0) {
     throw "zelph_tests failed with exit code $LASTEXITCODE"
 }
+#>
